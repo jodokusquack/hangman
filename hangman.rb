8241fc83a1ba7010a01645b3cbb7a80d10311fbf
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-03-11 23:23:03 +0100

    Made the game loop

--- hangman.rb
+++ hangman.rb
@@ -2,3 +2,6 @@
 
 # require all files under the lib directory
 Dir[File.join(__dir__, 'lib', '*.rb')].each { |file| require file }
+
+game = Game.new
+game.play
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,38 @@
 class Game
 
+  def inititalize()
+  end
+
+  def play()
+    continue = true
+    while continue == true
+      system "clear"
+      start_round
+      play_round
+      continue = continue?
+    end
+  end
+
+  private
+
+  def start_round()
+    #@player = create_player
+    #@secret_word = create_secret_word
+    puts "Setting up a game..."
+  end
+
+  def play_round()
+    puts "Playing the game. In my head..."
+  end
+
+  def continue?
+    puts "Do you want to play another round? [Y/n]"
+    input = gets.chomp.downcase
+
+    if ["n", "q", "e"].include? input[0]
+      false
+    else
+      true
+    end
+  end
 end
